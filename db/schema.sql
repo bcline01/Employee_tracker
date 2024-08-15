181c0172d5ef3b987b3217b5affa9e356f8ad36c
bcline01 <brookecline.bc@gmail.com>
2024-08-14 20:46:36 -0400

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS movies_db;
+CREATE DATABASE movies_db;
+
+\c movies_db;
+
+/*//TODO: Create a movie table */
+CREATE TABLE movies (
+    id SERIAL PRIMARY KEY,
+    movie_name VARCHAR(100) NOT NULL
+);
+
+/*//TODO: Create a review table */
+CREATE TABLE reviews (
+id SERIAL PRIMARY KEY,
+movie_id INTEGER,
+review TEXT NOT NULL,
+FOREIGN KEY (movie_id)
+REFERENCES movies(id)
+ON DELETE SET NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO movies (movie_name)
+VALUES 
+    ('The Italian Job'),
+    ('Braveheart'),
+    ('The Proposal'),
+    ('Spiderman'),
+    ('The Bourne Identity');
+
+
+INSERT INTO reviews (movie_id, review)
+VALUES (1, 'great'),
+        (2, 'awesome'),
+        (3, 'funny'),
+        (4, 'cool'),
+        (5, 'amazing');
