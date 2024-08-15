562c2d6405618089dbf25b79063417ab8b4e8d5d
bcline01 <brookecline.bc@gmail.com>
2024-08-14 21:52:03 -0400

    updated schema and seeds sql

--- db/schema.sql
+++ db/schema.sql
@@ -3,18 +3,31 @@ CREATE DATABASE movies_db;
 
 \c movies_db;
 
-/*//TODO: Create a movie table */
-CREATE TABLE movies (
+/*//TODO: Create a department table */
+CREATE TABLE department (
     id SERIAL PRIMARY KEY,
-    movie_name VARCHAR(100) NOT NULL
+    department_name VARCHAR(30) UNIQUE NOT NULL
 );
 
-/*//TODO: Create a review table */
-CREATE TABLE reviews (
+/*//TODO: Create a role table */
+CREATE TABLE roles (
 id SERIAL PRIMARY KEY,
-movie_id INTEGER,
-review TEXT NOT NULL,
-FOREIGN KEY (movie_id)
-REFERENCES movies(id)
+title VARCHAR(30) UNIQUE NOT NULL,
+salary DECIMAL NOT NULL,
+department_id INTEGER NOT NULL,
+FOREIGN KEY (department_id)
+REFERENCES department(id)
+ON DELETE SET NULL
+);
+
+/*//TODO: Create a employee table */
+CREATE TABLE employee (
+id SERIAL PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INTEGER NOT NULL,
+manager_id INTEGER,
+FOREIGN KEY (role_id)
+REFERENCES roles(id)
 ON DELETE SET NULL
 );
